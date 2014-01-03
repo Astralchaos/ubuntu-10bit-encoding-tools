fb9a94e96ff0cc2ef7a19cc20309d4047a1810c2
mmstick <mmstickman@gmail.com>
2014-01-03 17:19:51 -0600

    You may now invoke several scripts with nautilus

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # First, if the user has already run this before, make sure all the previous encode-* scripts are deleted.
-sudo rm /usr/local/bin/encode-*
+sudo rm /usr/local/bin/encode-* ~/.local/share/nautilus/scripts/encode*
 # Then, install the scripts
 chmod +x scripts/*
 cp scripts/encode-audio scripts/encode-music scripts/encode-video scripts/encode-video-parallel scripts/encode-merge ~/.local/share/nautilus/scripts
