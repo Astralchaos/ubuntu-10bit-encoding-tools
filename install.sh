7c6e9d2c0d90ccb66746521aea87d1816706b611
Michael Murphy <mmstickman@gmail.com>
2014-01-04 21:52:01 -0600

    Lots of fixes for interoptability between CLI and GUI usage.

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # First, if the user has already run this before, make sure all the previous encode-* scripts are deleted.
-sudo rm /usr/local/bin/encode-* ~/.local/share/nautilus/scripts/encode*
+sudo rm /usr/local/bin/encode-*
 # Then, install the scripts
-chmod +x scripts/*
-cp scripts/encode-audio scripts/encode-music scripts/encode-video scripts/encode-video-parallel scripts/encode-merge ~/.local/share/nautilus/scripts
 sudo install scripts/* /usr/local/bin
+install nautilus-scripts/* ~/.local/nautilus/scripts 
 # Then, run encode-compile which compiles/installs everything.
 encode-compile
